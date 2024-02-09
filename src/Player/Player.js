046b93424379c7e6a8252bbca39f2e62526f8c50
juanda <juandacorreo@gmail.com>
2024-02-09 13:53:32 +0100

    Añadida lógica de clases activa y winner

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -2,8 +2,26 @@ import './Player.css'
 // props = {name: 'Player 1', score: 43, current: 0, isActive: true}
 function Player({ name, score, current, isActive }) {
   // const { name, score, current, isActive } = props
+
+  // let classPlayer
+  // if (isActive) {
+  //   if (score >= 100) {
+  //     classPlayer = 'player player--winner'
+  //   } else {
+  //     classPlayer = 'player player--active'
+  //   }
+  // } else {
+  //   classPlayer = 'player'
+  // }
+
+  const classPlayer = isActive
+    ? score > 100
+      ? 'player player--winner'
+      : 'player player--active'
+    : 'player'
+
   return (
-    <section className="player player--active">
+    <section className={classPlayer}>
       <h2 className="name">{name}</h2>
       <p className="score">{score}</p>
       <div className="current">
